5a774a646339ba5e8fa49b2a7f8bf6c95af377ba
Raphael Lullis <raphael@lullis.net>
2012-12-17 16:03:29 -0500

    Needed to add 'packages' parameter to setup.py.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.2012',
     description='A client library for Parse.com\'.s REST API',
     url='https://github.com/dgrtwo/ParsePy',
+    packages=['parse'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
