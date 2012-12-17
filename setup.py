25b176f7c7e6bf5eaea2aff372e3aeabc9d64711
Raphael Lullis <raphael@lullis.net>
2012-12-17 15:09:38 -0500

    Adding a very basic setup.py file to make it pip-installable.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='ParsePy',
+    version='0.0.2012',
+    description='A client library for Parse.com\'.s REST API',
+    url='https://github.com/dgrtwo/ParsePy',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPLv3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+        ]
+    )
