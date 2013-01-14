bc7aa3cd6d795efcaff020fa7d6a0c56a72dc6b3
David Robinson <dgrtwo@princeton.edu>
2013-01-14 16:07:30 -0500

    Some modifications for uploading to PyPI, to setup.py and to .gitignore

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ setup(
     description='A client library for Parse.com\'.s REST API',
     url='https://github.com/dgrtwo/ParsePy',
     packages=['parse_rest'],
+    maintainer="David Robinson",
+    maintainer_email="dgrtwo@princeton.edu",
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GPLv3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python'
         ]
