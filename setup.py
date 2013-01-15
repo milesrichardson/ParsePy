895cb6888af99ff799632b8ab720d8886d65f823
David Robinson <dgrtwo@princeton.edu>
2013-01-15 01:38:00 -0500

    Added installation information to README. Added 'python setup.py test' command.

--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
-from distutils.core import setup
+from distutils.core import setup, Command
+from unittest import TextTestRunner, TestLoader
+
+
+class TestCommand(Command):
+    """Run test suite using 'python setup.py test'"""
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """Run test suite in parse_rest.tests"""
+        try:
+            from parse_rest import tests
+        except ImportError:
+            raise Exception("parse_rest is not installed, cannot run tests")
+        tests = TestLoader().loadTestsFromNames(["parse_rest.tests"])
+        t = TextTestRunner(verbosity=1)
+        t.run(tests)
+
 
 setup(
     name='parse_rest',
-    version='0.0.2012',
+    version='0.1.2012',
     description='A client library for Parse.com\'.s REST API',
     url='https://github.com/dgrtwo/ParsePy',
     packages=['parse_rest'],
     maintainer="David Robinson",
     maintainer_email="dgrtwo@princeton.edu",
+    cmdclass={"test": TestCommand},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
