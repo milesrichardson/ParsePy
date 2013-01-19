036b1353e8021eb5c2abd0f21aa1c97c866cdb70
David Robinson <dgrtwo@princeton.edu>
2013-01-19 15:04:07 -0500

    Updated version number for PyPI

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class TestCommand(Command):
 
 setup(
     name='parse_rest',
-    version='0.1.2012',
+    version='0.2.2012',
     description='A client library for Parse.com\'.s REST API',
     url='https://github.com/dgrtwo/ParsePy',
     packages=['parse_rest'],
