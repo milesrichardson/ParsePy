d9506152d04291d27fecc22da3e196a162b947ee
Marlon <branford05@gmail.com>
2015-01-28 18:55:12 -0600

    Handle adding and removing relations by User.

--- parse_rest/user.py
+++ parse_rest/user.py
@@ -113,6 +113,28 @@ class User(ParseResource):
 
     def __repr__(self):
         return '<User:%s (Id %s)>' % (getattr(self, 'username', None), self.objectId)
+    
+    def removeRelation(self, key, className, objectsId):
+        self.manageRelation('RemoveRelation', key, className, objectsId)
+
+    def addRelation(self, key, className, objectsId):
+        self.manageRelation('AddRelation', key, className, objectsId)
+
+    def manageRelation(self, action, key, className, objectsId):
+        objects = [{
+                    "__type": "Pointer",
+                    "className": className,
+                    "objectId": objectId
+                    } for objectId in objectsId]
+
+        payload = {
+            key: {
+                 "__op": action,
+                 "objects": objects
+                }
+            }
+        self.__class__.PUT(self._absolute_url, **payload)
+        self.__dict__[key] = ''
 
 
 User.Query = QueryManager(User)
