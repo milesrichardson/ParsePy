acf2290b7f14f303b071ce8c8df91207c2791500
Dan Robinson <danrobinson010@gmail.com>
2015-04-13 09:11:52 -0400

    Revert "Set an ACL on ParseResource by default"
    Default ACL breaks some functionalities.
    
    This reverts commit eb437d9849592e985f195083e9de7a7ed08b45d1.
    
    Closes #97

--- parse_rest/datatypes.py
+++ parse_rest/datatypes.py
@@ -255,8 +255,6 @@ class ACL(ParseType):
 
     def __init__(self, acl=None):
         self._acl = acl or {}
-        if acl is None:
-            self.set_default(read=True, write=True)
 
     def _to_native(self):
         return self._acl
@@ -319,8 +317,6 @@ class ParseResource(ParseBase):
     def __init__(self, **kw):
         self.objectId = None
         self._init_attrs(kw)
-        if 'ACL' not in kw:
-            self.ACL = ACL()
 
     def __getattr__(self, attr):
         # if object is not loaded and attribute is missing, try to load it
